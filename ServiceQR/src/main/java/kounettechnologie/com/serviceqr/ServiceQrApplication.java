f9c03e6011c4650f1014c490742cfe5fce4c38f2
kounet95 <85202452+kounet95@users.noreply.github.com>
2025-02-04 00:46:41 -0500

    ff

---
+++ ServiceQR/src/main/java/kounettechnologie/com/serviceqr/ServiceQrApplication.java
@@ -0,0 +1,13 @@
+package kounettechnologie.com.serviceqr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceQrApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceQrApplication.class, args);
+    }
+
+}
---
+++ ServiceQR/src/test/java/kounettechnologie/com/serviceqr/ServiceQrApplicationTests.java
@@ -0,0 +1,13 @@
+package kounettechnologie.com.serviceqr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceQrApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
